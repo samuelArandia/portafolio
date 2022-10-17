a64fc56f7ea6a4dd31ec69ebafd98d9edce4b32b
samuel <samuelarandia@gmail.com>
2022-10-17 01:35:49 -0300

    footer y nav check

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const bdark = document.querySelector('#bdark');
 const body = document.querySelector('body');
+const mood = document.querySelector('#mood');
 
 load();
 
 bdark.addEventListener('click', () => { 
     body.classList.toggle('darkmode');
     store(body.classList.contains('darkmode'));
+    bdark.innerHTML = body.classList.contains('darkmode') ? '<i id="mood" class="fa-regular fa-sun"></i>' : '<i id="mood" class="fa-regular fa-moon"></i>';
 });
 
 function load(){
@@ -20,3 +22,10 @@ function load(){
 function store(value){
   localStorage.setItem('darkmode', value);
 }
+
+
+document.getElementById("icon_menu").addEventListener("click", show_nav);
+
+function show_nav(){
+    document.querySelector(".nav").classList.toggle("show_nav");
+};
